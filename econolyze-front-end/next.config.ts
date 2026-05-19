56e4d416c39b56097998c89cc6a8835c5b00bb9b
Erick Murakami <erickmurakami.ehm@gmail.com>
2026-05-19 14:15:49 -0300

    fix(url): removed url from local dev

--- econolyze-front-end/next.config.ts
+++ econolyze-front-end/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
     env:{
         // NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-        NEXT_PUBLIC_API_BASE_URL: "http://192.168.3.32:8080/api",
+        // NEXT_PUBLIC_API_BASE_URL: "http://192.168.3.32:8080/api",
     }
 };
 
